aa045aead905693940cd11c48df775d9440502fc
Marc Backes <mab@mab.lu>
2019-12-05 17:52:04 +0100

    Access to hidden pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,27 +6,36 @@ module.exports.createPages = async ({ graphql, actions }) => {
 
     const res = await graphql(`
         query {
-            allContentfulBlogPost(
-                sort: { fields: publishedAt, order: DESC }
-            ) {
+            allContentfulBlogPost(sort: { fields: publishedAt, order: DESC }) {
                 edges {
                     node {
                         title
                         slug
                         publishedAt(formatString: "MMMM Do, YYYY")
+                        hideFromList
                     }
                 }
             }
         }
     `)
 
-    res.data.allContentfulBlogPost.edges.forEach(({node}) => {
-        createPage({
-            component: blogTemplate,
-            path: `/blog/${node.slug}`,
-            context: {
-                slug: node.slug,
-            },
-        })
+    res.data.allContentfulBlogPost.edges.forEach(({ node }) => {
+        if (node.hideFromList === true) {
+            createPage({
+                component: blogTemplate,
+                path: `/${node.slug}`,
+                context: {
+                    slug: node.slug,
+                },
+            })
+        } else {
+            createPage({
+                component: blogTemplate,
+                path: `/blog/${node.slug}`,
+                context: {
+                    slug: node.slug,
+                },
+            })
+        }
     })
 }
