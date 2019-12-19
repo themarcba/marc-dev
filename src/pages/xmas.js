0f061eef226b5ee274f04de47707adfe5ceb740d
Marc Backes <mab@mab.lu>
2019-12-19 10:27:49 +0100

    removed tracedSVG from xmas image

--- src/pages/xmas.js
+++ src/pages/xmas.js
@@ -12,7 +12,6 @@ export const query = graphql`
             childImageSharp {
                 fluid {
                     base64
-                    tracedSVG
                     aspectRatio
                     src
                     srcSet
