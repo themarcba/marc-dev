a60cbffc9d6b39443f014a1af79a9a618d374de9
Marc Backes <mab@mab.lu>
2019-12-31 09:04:17 +0100

    set header text for real hostname (in case called from my other domain)

--- src/components/header.js
+++ src/components/header.js
@@ -20,6 +20,7 @@ const Header = () => {
         }
     `)
 
+    const hostname = window.location.hostname === 'localhost' ? 'marc.dev' : window.location.hostname
     const pages = data.allSitePage.edges.map(edge =>
         edge.node.path.replace(/\/$/, "")
     )
@@ -58,7 +59,7 @@ const Header = () => {
             </div>
             <h1>
                 <Link className={headerStyles.title} to="/">
-                    marc.dev
+                    {hostname}
                 </Link>
                 {location}
             </h1>
