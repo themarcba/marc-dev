17ac48799311979a2d4d3226db50618cd0b2fe7a
Marc Backes <mab@mab.lu>
2020-01-02 12:13:11 +0100

    added text to redirect page

--- src/templates/redirect.js
+++ src/templates/redirect.js
@@ -1,11 +1,16 @@
 import React from "react"
+import mainStyles from "../styles/main.module.scss"
 
 class Redirect extends React.Component {
     componentDidMount() {
         window.location = this.props.pageContext.url
     }
     render() {
-        return <></>
+        return (
+            <div className={mainStyles.center}>
+                <h1>You are being redirected...</h1>
+            </div>
+        )
     }
 }
 
